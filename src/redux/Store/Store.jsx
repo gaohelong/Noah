3f240e057b4965548888772f65cc2a0c38af0717
helong.gao <helong.gao@sinobbd.com>
2017-12-26 17:19:01 +0800

    Noah: redux devTools Config

--- src/redux/Store/Store.jsx
+++ src/redux/Store/Store.jsx
@@ -1,10 +1,11 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import * as reducer from '../Reducers/index';
 import thunk from 'redux-thunk';
 
 const store = createStore(
     combineReducers(reducer),
-    applyMiddleware(thunk)
+    // applyMiddleware(thunk),
+    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;
